6768faf891dbdc7785e06c4e73233dd84a55f25c
ForgedExistence <47781489+ForgedExistence@users.noreply.github.com>
2020-02-01 14:02:31 +0000

    More Updated stuff
    got rid of some unnessecary parts of code still needs work itself but getting there.

--- test2.py
+++ test2.py
@@ -1,78 +1,30 @@
-# Right so what happened with the first test was weird how can I get around it
-# I can start in a similar manner but change things
-# priority is getting a sim running well enough to show circular orbits
-import math
-from turtle import *
+import numpy as np
+from matplotlib import pyplot as plt
+from matplotlib import animation
 
+fig = plt.figure()
+fig.set_dpi(100)
+fig.set_size_inches(7, 6.5)
 
-# I got a large amount of info from NASA links in README
+ax = plt.axes(xlim=(-5, 5), ylim=(-5, 5))
+patch = plt.Circle((5, -5), 0.75, fc='y')
 
-# the formula we will be using is from Newtons Law of gravitation
-# firstly some constants and stuff
+def init():
+    patch.center = (5, -5)
+    ax.add_patch(patch)
+    return patch,
 
-G = 6.67e-11  # m^3 kg^-1 s^-2
+def animate(i):
+    x, y = patch.center
+    x =  np.sin(np.radians(i))
+    y =  np.cos(np.radians(i))
+    patch.center = (x, y)
+    return patch,
 
-# Radii of planets and sun
-# For simplicity sun is of radius 0 and doesnt move
-sun_r = 0
-mercury_r = 57.9e+9
-venus_r = 108.2e+9
-earth_r = 149.6e+9
-mars_r = 227.9e+9
+anim = animation.FuncAnimation(fig, animate,
+                               init_func=init,
+                               frames=360,
+                               interval=20,
+                               blit=True)
 
-# masses
-sun_m = 1.99e+30
-mercury_m = 0.33e+24
-venus_m = 4.87e+24
-earth_m = 5.97e+24
-mars_m = 0.642e+24
-
-# we know their average velocities
-# all in m/s
-sun_v = 0
-mercury_v = 47.4e+3
-venus_v = 35e+3
-earth_v = 29e+3
-mars_v = 24.1e+3
-
-# all of our important data is here so whats next?
-# hmmm
-# ah yes we should prolly increase the tear count on this project and split up
-# the velocities into x and y directions
-# need a loop to compute the velocities
-
-bodies_v = [mercury_v, venus_v, earth_v, mars_v]
-bodies_r = [mercury_r, venus_r, earth_r, mars_r]
-bodies_m = [mercury_m, venus_m, earth_m, mars_m]
-
-posx, posy = 0, 0
-mass = None
-
-# how do i split up the velocities into x and y????
-
-# I guess because of the bodies moving in a circle the
-# x and y velocities are
-
-# lets do it for one planet first
-
-
-# def attraction(Body, Sun):
-#     if body is Sun:
-#         # need to stop objects atracting to themselves
-#         raise ValueError("Attraction to itself" % body.name)
-#     # all bodies will be taken from the centre of the solar system i.e.
-#       the sun distance will be from the object to the sun always
-#     sx, sy = Body.posx, Body.posy
-#     d = math.sqrt(sx**2 + sy**2)
-#
-#     if d == 0:
-#         raise ValueError("Abandon ship")
-#
-#     f = (G*sun_m*Body.mass)/(d**2)
-#
-#     theta = math.atan2(sx, sy)
-#     fx = f*math.cos(theta)
-#     fy = f*math.sin(theta)
-#     return fx, fy
-#
-# # right i need to
+plt.show()
--- test3.py
+++ test3.py
@@ -2,9 +2,15 @@
 # lets start by making a class system so the data
 # can be used easily
 
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+
 
 class Body:
     solar_sys = []
+    s_day = 24*60**2
 
     def __init__(self, mass, name, velocity, radius):
         self.mass = mass
@@ -12,29 +18,94 @@ class Body:
         self.velocity = velocity
         self.radius = radius
         self.id = len(Body.solar_sys)
+        self.angular_v = self.velocity/self.radius
         Body.solar_sys.append(self)
+        self.theta = self.get_theta()
 
     def __repr__(self):
         my_rep = f"""
         Body name: {self.name}
-        Bodyid: {self.id}
+        Body ID: {self.id}
         Body mass: {self.mass}
         Body velocity: {self.velocity}
         Body radius: {self.radius}
+        Body angular_v: {self.angular_v}
+        Body theta: {self.theta}
         --------------
         """
         return my_rep
 
+    def get_theta(self):
+        theta = (self.velocity*Body.s_day)/self.radius
+        return theta
 
-Body(1.99e+30, "Sun", 0, 0)
-Body(0.33e+24, "Mercury", 47.4e+3, 57.9e+9)
-Body(4.87e+24, "Venus", 35e+3, 108.2e+9)
-Body(5.97e+24, "Earth", 29e+3, 149.6e+9)
-Body(0.642e+24, "Mars", 24.1e+3, 227.9e+9)
+
+# Sun = Body(1.99e+30, "Sun", 0, 0)
+Body(0.33e+24, "Mercury", 47.4e+3, 0.387)
+Body(4.87e+24, "Venus", 35e+3, 0.723)
+Body(5.97e+24, "Earth", 29e+3, 1)
+Body(0.642e+24, "Mars", 24.1e+3, 1.523)
 
 input()
+
 for i in Body.solar_sys:
     print(i)
-# Time for fun
+# # Time for fun
 # Use matplotlib
 # Look up planetary motion online
+#v=rw where v= velocity r= radius w= angular velocity
+
+# What I want to do is change the coords using the angular velocity
+# I will make a def and for each time t change the angle of the
+# orbit
+
+#Sun
+# time = 24*60**2
+# theta = 2
+# radius = 0
+# fig = plt.figure()
+# ax = plt.axes(xlim = (-3, 3), ylim = (-3, 3))
+# patch = plt.Circle((.1, -.1), 0.075, fc='y')
+#
+#
+# def init():
+#     patch.center = (0, 0)
+#     ax.add_patch(patch)
+#     return patch,
+# def animate(i):
+#     posx, posy = patch.center
+#     posx = radius * np.cos(theta * time * i)
+#     posy = radius * np.sin(theta * time * i)
+#     patch.center = (posx, posy)
+#
+#     return patch,
+# anim = animation.FuncAnimation(fig, animate,
+#                                init_func=init,
+#                                frames=360,
+#                                interval=20,
+#                                blit=True)
+time = 24*60**2
+theta = 2
+radius = 0.387
+fig = plt.figure()
+ax = plt.axes(xlim = (-3, 3), ylim = (-3, 3))
+patch = plt.Circle((.1, -.10), 0.075, fc='b')
+
+
+def init():
+    patch.center = (0, 0)
+    ax.add_patch(patch)
+    return patch,
+def animate(i):
+    posx, posy = patch.center
+    posx = radius * np.cos(theta * time * i)
+    posy = radius * np.sin(theta * time * i)
+    patch.center = (posx, posy)
+
+    return patch,
+anim = animation.FuncAnimation(fig, animate,
+                               init_func=init,
+                               frames=360,
+                               interval=20,
+                               blit=True)
+plt.show()
